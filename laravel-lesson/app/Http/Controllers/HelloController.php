d4abd4ea4f1c5164475b3337dd461db2fb6692ce
MasanoriOno <masanoriono@A218.local>
2022-10-09 18:09:24 +0900

    Bladeを使用してPOSTを試す

--- laravel-lesson/app/Http/Controllers/HelloController.php
+++ laravel-lesson/app/Http/Controllers/HelloController.php
@@ -10,7 +10,16 @@ class HelloController extends Controller
     public function index(Request $request)
     {
         $data = [
-            'msg' => 'これはBladeサンプルです',
+            'msg' => '名前を入力して',
+        ];
+        return view('hello.index', $data);
+    }
+
+    public function post(Request $request)
+    {
+        $msg = $request->msg;
+        $data = [
+            'msg' => 'こんにちは' . $msg . 'さん'
         ];
         return view('hello.index', $data);
     }
