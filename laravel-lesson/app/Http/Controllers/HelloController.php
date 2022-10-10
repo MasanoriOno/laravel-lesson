62fa5f0043c9535c37d368f4069200759a290b23
monomonosu <pojinyan@yahoo.co.jp>
2022-10-10 18:52:09 +0900

    クエリー文字列のバリデーター作成

--- laravel-lesson/app/Http/Controllers/HelloController.php
+++ laravel-lesson/app/Http/Controllers/HelloController.php
@@ -11,17 +11,25 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        return view('hello.index', ['msg' => 'フォームを入力']);
+        $validator = Validator::make($request->query(), [
+            'id' => 'required',
+            'pass' => 'required',
+        ]);
+        if ($validator->fails())
+            $msg = 'クエリー文字列に問題';
+        else
+            $msg = 'ID,PASS OK!';
+        return view('hello.index', ['msg' => $msg]);
     }
 
     public function post(Request $request)
     {
-        $validator=Validator::make($request->all(),[
-            'name'=>'required',
-            'mail'=>'email',
-            'age'=>'numeric|between:0,150',
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric|between:0,150',
         ]);
-        if($validator->fails()){
+        if ($validator->fails()) {
             return redirect('/hello')->withErrors($validator)->withInput();
         }
 
