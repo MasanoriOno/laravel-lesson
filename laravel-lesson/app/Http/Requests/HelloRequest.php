0b8e39562171a2a024967ef1ef5a0779e5c997e7
monomonosu <pojinyan@yahoo.co.jp>
2022-10-10 19:56:04 +0900

    独自Ruleを作ってみる

--- laravel-lesson/app/Http/Requests/HelloRequest.php
+++ laravel-lesson/app/Http/Requests/HelloRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Rules\MyRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class HelloRequest extends FormRequest
@@ -29,7 +30,7 @@ class HelloRequest extends FormRequest
         return [
             'name' => 'required',
             'mail' => 'email',
-            'age' => 'numeric|hello',
+            'age' => ['numeric', new MyRule(5)],
         ];
     }
 
