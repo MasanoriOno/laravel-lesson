b84002f7342a4dfe90a2778ffdc366e1e262edae
monomonosu <pojinyan@yahoo.co.jp>
2022-10-16 20:30:37 +0900

    クエリビルダの基礎

--- laravel-lesson/app/Http/Controllers/HelloController.php
+++ laravel-lesson/app/Http/Controllers/HelloController.php
@@ -12,12 +12,7 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        if (isset($request->id)) {
-            $param = ['id' => $request->id];
-            $items = DB::select('select * from people where id = :id', $param);
-        } else {
-            $items = DB::select('select * from people');
-        }
+        $items = DB::table('people')->get();
         return view('hello.index', ['items' => $items]);
     }
 
@@ -25,6 +20,12 @@ class HelloController extends Controller
     {
         $items = DB::select('select * from people');
         return view('hello.index',['items'=>$items]);
+    }
+
+    public function show(Request $request){
+        $id = $request->id;
+        $item = DB::table('people')->where('id',$id)->first();
+        return view('hello.show',['item'=>$item]);
     }
 
     public function add(Request $request){
