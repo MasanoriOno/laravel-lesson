7b6d211437802c66068419ea71b9bf4be048d472
monomonosu <pojinyan@yahoo.co.jp>
2022-10-23 19:49:27 +0900

    モデルクラスのfindでIDによる検索を試す

--- laravel-lesson/routes/web.php
+++ laravel-lesson/routes/web.php
@@ -36,3 +36,5 @@ Route::get('/hello/del', [HelloController::class, 'del']);
 Route::post('/hello/del', [HelloController::class, 'remove']);
 
 Route::get('person', [PersonController::class, 'index']);
+Route::get('person/find', [PersonController::class, 'find']);
+Route::post('/person/find', [PersonController::class, 'search']);
